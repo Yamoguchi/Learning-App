2d607059faed146283a38e0c3be1c1c00cbbfa58
Rodion Mustafaev <rodion9116@yandex.ru>
2023-12-20 00:45:03 +1000

    Add project files.

---
+++ LearningApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LearningApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
