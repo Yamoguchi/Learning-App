c44d38fcc06c6074f795db502144d626828ef301
Rodion Mustafaev <rodion9116@yandex.ru>
2023-12-20 14:53:44 +1000

    Added new Class - Card

---
+++ LearningApp/Model/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningApp.Model
+{
+    class Card
+    {
+        public string Question {  get; private set; }
+        public string Answer { get; private set; }
+        
+        public Card(string question, string answer)
+        {
+            this.Question = question;
+            this.Answer = answer;
+        }
+
+        public void setQuestion(string question)
+        {
+            this.Question=question;
+        }
+
+        public void setAnswer(string answer)
+        {
+            this.Answer = answer;
+        }
+    }
+}
